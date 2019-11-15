143104fe108d47d0d0a20a4f48599b7eaec75b36
yates <1527708673@qq.com>
2019-11-15 13:29:18 +0800

    <source>is void element

--- src/views/lessons/play.vue
+++ src/views/lessons/play.vue
@@ -1,77 +1,74 @@
 <template>
-	<Layout class="layout">
-		<Content class="layout-content">
-			<div v-if="currentLessonStep.type === 'introduction'" class="layout-content-intro" >
-				<video controls autoplay>
-					<source :src="currentLessonStep.url" type="video/mp4"></source>
-					<h1>哎呀，视频不见了呢！</h1>
-				</video>
-			</div>
-			<div v-if="currentLessonStep.type === 'firstType'" class="layout-content-firstType">
-				<h1>First Typing of {{currentLessonStep.keys}}</h1>
-				<Keyboard></Keyboard>
-			</div>
-			<div v-if="currentLessonStep.type === 'exercise'" class="layout-content-exercise">
-				<h1>exercise of {{currentLessonStep.keys}}</h1>
-				<Keyboard></Keyboard>
-			</div>
-            <div v-if="currentLessonStep.type === 'game'" class="layout-content-exercise">
-				<h1>exercise of {{currentLessonStep.keys}}</h1>
-				<Keyboard></Keyboard>
-			</div>
-		</Content>
-	</Layout>
+  <Layout class="layout">
+    <Content class="layout-content">
+      <div v-if="currentLessonStep.type === 'introduction'" class="layout-content-intro">
+        <video controls autoplay>
+          <source :src="currentLessonStep.url" type="video/mp4" />
+          <h1>哎呀，视频不见了呢！</h1>
+        </video>
+      </div>
+      <div v-if="currentLessonStep.type === 'firstType'" class="layout-content-firstType">
+        <h1>First Typing of {{currentLessonStep.keys}}</h1>
+        <Keyboard></Keyboard>
+      </div>
+      <div v-if="currentLessonStep.type === 'exercise'" class="layout-content-exercise">
+        <h1>exercise of {{currentLessonStep.keys}}</h1>
+        <Keyboard></Keyboard>
+      </div>
+      <div v-if="currentLessonStep.type === 'game'" class="layout-content-exercise">
+        <h1>exercise of {{currentLessonStep.keys}}</h1>
+        <Keyboard></Keyboard>
+      </div>
+    </Content>
+  </Layout>
 </template>
 
 
 <script>
-	import lessonDatsFromServer from "../../static/lessonCards.js";
-	
-	export default{
-		components:{
-			
-		},
-		data(){
-			return {
-				currentLessonId: 0,
-				currentLessonData:{},
-				currentLessonStepId: 0,
-				currentLessonStep: {}
-			}
-		},
-		methods:{
-			getCurrentLessonData(){
-				this.currentLessonId = this.$store.state.currentLessonId;
-				return lessonDatsFromServer[this.currentLessonId];
-			},
-			getCurrentLessonStep(){
-				this.currentLessonStepId = this.$store.state.currentLessonStepId;
-				return this.currentLessonData[this.currentLessonStepId-1];
-			}
-		},
-		mounted(){
-			this.currentLessonData = this.getCurrentLessonData();
-			this.currentLessonStep = this.getCurrentLessonStep();
-		}
-	}
+import lessonDatsFromServer from "../../static/lessonCards.js";
+
+export default {
+  data() {
+    return {
+      currentLessonId: 0,
+      currentLessonData: {},
+      currentLessonStepId: 0,
+      currentLessonStep: {}
+    };
+  },
+  methods: {
+    getCurrentLessonData() {
+      this.currentLessonId = this.$store.state.currentLessonId;
+      return lessonDatsFromServer[this.currentLessonId];
+    },
+    getCurrentLessonStep() {
+      this.currentLessonStepId = this.$store.state.currentLessonStepId;
+      return this.currentLessonData[this.currentLessonStepId - 1];
+    }
+  },
+  mounted() {
+    this.currentLessonData = this.getCurrentLessonData();
+    this.currentLessonStep = this.getCurrentLessonStep();
+  }
+};
 </script>
 
 
 <style scoped lang="less">
-	@myBorder: lightblue 4px solid;
-	.layout{
-		.layout-content{
-			min-height: 620px;
-			display: flex;
-			justify-content: center;
-			.layout-content-intro{
-				padding: 10px;
-				@myBorder: lightblue 2px solid;
-				video{
-					border-radius: 4px;
-					min-height: 620px;
-				}
-			}
-		}
-	}
+@myBorder: lightblue 4px solid;
+.layout {
+  .layout-content {
+    min-height: 620px;
+    display: flex;
+    justify-content: center;
+    .layout-content-intro {
+      padding: 10px;
+      @myBorder: lightblue 2px solid;
+      video {
+        border-radius: 4px;
+        min-height: 620px;
+      }
+    }
+  }
+}
 </style>
