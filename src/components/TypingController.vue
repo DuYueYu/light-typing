7975c19819cab6dde27c4ea3dadb40d2fa347629
yates <1527708673@qq.com>
2019-12-05 12:23:33 +0800

    新增样式

--- src/components/TypingController.vue
+++ src/components/TypingController.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <div>
-      <Key v-for="(char, index) in data" :char="char" :key="index"></Key>
+  <div class="content">
+    <div class="contnet-key">
+      <Key v-for="(char, index) in data" :char="char" :key="index" :id="index"></Key>
     </div>
-    <Keyboard @keydown="handleKeydown"></Keyboard>
+    <Keyboard @keydown="handleKeydown" class="contnet-keyboard"></Keyboard>
   </div>
 </template>
 
@@ -15,21 +15,45 @@ export default {
       default: []
     }
   },
-
+  data() {
+    return {
+      index: 0
+    };
+  },
   methods: {
     handleKeydown(key) {
-      console.log("key = ", key);
-      console.log("this.data = ", this.data);
-      if (key === this.data[0]) {
-        this.data.shift();
-        if (this.data.length === 0) {
-          this.$emit("finished");
-        }
+      if (key === this.data[this.index]) {
+        document.getElementById(`${this.index}`).style.background = "lightblue";
+      } else {
+        document.getElementById(`${this.index}`).style.background = "#ff9944";
       }
+      if (this.data.length === this.index + 1) {
+        this.$emit("finished");
+      }
+      this.index++;
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+@myBorder: blue 2px solid;
+
+.content {
+  position: relative;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  border: @myBorder;
+  .contnet-key {
+    width: 826px;
+    height: 30%;
+    border: @myBorder;
+  }
+  .contnet-keyboard {
+    position: absolute;
+    bottom: 10%;
+    border: @myBorder;
+  }
+}
 </style>
