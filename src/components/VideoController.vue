485b12fa2313cd627731b86d73a63a8f4ed17252
yates <1527708673@qq.com>
2019-12-05 12:23:44 +0800

    新增组件

--- src/components/VideoController.vue
+++ src/components/VideoController.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    
+  <div class="content">
+    <video controls autoplay>
+      <source :src="url" type="video/mp4" />
+      <h1>哎呀，视频不见了呢！</h1>
+    </video>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    url: ""
+  }
+};
 </script>
 
 <style lang="less" scoped>
+@myBorder: 2px solid blue;
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  video {
+    border-radius: 4px;
+    min-height: 620px;
+  }
+}
 </style>
